732098bf18cac8849855d1642bc0308b8a23dcf9
FBIMOBILEUNIT\D'Ann <me@dnhart.com>
2016-11-29 09:43:55 -0600

    working code. basic debugging. pretty much done.

--- assets/javascript/giphyapi.js
+++ assets/javascript/giphyapi.js
@@ -1,50 +1,80 @@
-var topics=["happy","sad", "excited", "happy dance", "confused", "nervous", "annoyed", "mind blown", "silly", "love", "innocent", "laughing", "hillarious", "sleepy", "shocked"];  
+var topics=["angry","bored", "confused", "happy dance", "unimpressed", "good job", "bye", "mind blown", "eye roll", "inspired", "no", "yes", "whatever", "thank you", "shocked"];  
+
+var searchTerm;
+
 
 function setup(){
-$.each(topics, function(i, value) {
-	var newButton =$("<button type='button' class='btn btn-info buttonSearch' value='"+value+"'>"+value+'</button>');
-	var buttonBox =$("#buttonBox");
-	buttonBox.append(newButton);
-});
+	$("#buttonBox").html("");
+	$.each(topics, function(i, value) {
+		var newButton =$("<button type='button' class='btn btn-info buttonSearch' value='"+value+"'>"+value+'</button>');
+		var buttonBox =$("#buttonBox");
+		buttonBox.append(newButton);
+	});
 
+	//assign onClick functionality to display gifs from Giphy when topic buton is clicked.
+	$(".buttonSearch").on("click", buttonSearch);
 };
 
 
 //setup initial buttons from the topics array
 setup();
 
-//assign onClick functionality to display gifs from Giphy when topic buton is clicked.
-$(".buttonSearch").on("click", displayGifs);
 
-function displayGifs (){
-	var searchTerm = this.attributes[2].value; 
+//sets searchTerm when topic button is clicked
+function buttonSearch(){
+	searchTerm = this.attributes[2].value; 
 	searchTerm = searchTerm.replace(/\s+/g, '+').toLowerCase();
 		console.log(searchTerm);
-    // Example queryURL for Giphy API
-   var queryURL = "http://api.giphy.com/v1/gifs/search?q="+searchTerm+"&api_key=dc6zaTOxFJmzC&limit=10";
+  	displayGifs ();
+};
 
+//assigns onClick function to search button
+$(".userInput").on("click", userSearch);
 
+//sets searchTerm when the user enters a term in the search box
+function userSearch(e){
+	e.preventDefault();
+	searchTerm = $('#search').val(); 
+	searchTerm = searchTerm.replace(/\s+/g, '+').toLowerCase();
+		console.log(searchTerm);
+	topics.push(searchTerm);
+	setup();
+   	displayGifs ();    
+   	$('#search').val('').placeholder();
+};
+
+//takes the searchTerm, queries Giphy, and displays results.
+function displayGifs (){
+	//giphy query
+   var queryURL = "http://api.giphy.com/v1/gifs/search?q="+searchTerm+"&api_key=dc6zaTOxFJmzC&rating=pg-13&limit=10";
+
+   //pulls the inforation (still image, animated image, rating, and slug) from the gighy JSON object
   $.ajax({ url: queryURL, method: 'GET' }).done(function(response) {
       var giphyArray = response;
       var importArray =giphyArray.data;
       console.log(importArray);
+
+      //clears the existing images, if any
+      $("#photoContainer").html("");
+
+      //pulls the variables and displays the images from the JSON object
       $.each(importArray, function(key, value){
 		var whatisthekey = key;
 		var stillImage = value.images.fixed_height_still.url;
 		var animatedImage = value.images.fixed_height.url;
+		var rating = value.rating;
 		var slug = value.slug;
-		$("#photoContainer").append("<div class='portfolio-item'><img class='img-responsive still' src="+stillImage+" alt='"+slug+"'><img class='img-responsive animated' src="+animatedImage+" alt='"+slug+"'></div>");
+		$("#photoContainer").append("<div class='portfolio-item'><img class='img-responsive still' src="+stillImage+" alt='"+slug+"'><img class='img-responsive animated' src="+animatedImage+" alt='"+slug+"'><figcaption>Rating: "+rating+"</div>");
 
 	});
+    //assigns onClick functionality to images
+  	$(".portfolio-item").on("click", toggleGifs);   
 
-   $(".portfolio-item").on("click", toggleGifs);   
+  }); //end ajax loop
+};//end displayGifs
 
-  });
-
-//});
-};
-
+//toggles visibiity on still and animated images
 function toggleGifs (event){
 	$(event.currentTarget.firstChild).toggle();
-	$(event.currentTarget.lastChild).toggle();
+	$(event.currentTarget.childNodes[1]).toggle();
 };
